ff32e782257d197ad591afe7cbc10a6d0eb78bf0
ericp <eric.pattinson@hotmail.com>
2024-02-01 11:49:00 +0100

    Version 1.0

---
+++ Calculator-IOS/Methods.swift
@@ -0,0 +1,8 @@
+//
+//  Methods.swift
+//  Calculator-IOS
+//
+//  Created by Eric on 19/1/24.
+//
+
+import Foundation
---
+++ Calculator-IOS/UIButtonExtension.swift
@@ -0,0 +1,8 @@
+//
+//  UIButtonExtension.swift
+//  Calculator-IOS
+//
+//  Created by Eric on 19/1/24.
+//
+
+import Foundation
