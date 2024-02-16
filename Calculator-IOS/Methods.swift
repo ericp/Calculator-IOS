419d840356e808749530b9e954988980a19966c8
ericp <eric.pattinson@hotmail.com>
2024-02-16 09:50:33 +0100

    First Version 1.0

--- Calculator-IOS/Methods.swift
+++
@@ -1,8 +0,0 @@
-//
-//  Methods.swift
-//  Calculator-IOS
-//
-//  Created by Eric on 19/1/24.
-//
-
-import Foundation
---
+++ Calculator-IOS/Model.swift
@@ -0,0 +1,8 @@
+//
+//  Model.swift
+//  Calculator-IOS
+//
+//  Created by Eric on 4/2/24.
+//
+
+import Foundation
